36800b6c24e380a872b41f47c026d37160052e40
saykishish <saykishish@gmail.com>
2018-03-08 15:42:13 +0800

    2018-03-08 03:37pm

--- app/src/main/java/com/example/asus/guessnumberv1/MainActivity.java
+++ app/src/main/java/com/example/asus/guessnumberv1/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.asus.guessnumberv1;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button guess;
+    private EditText input;
+    private TextView log, mesg;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        guess = findViewById(R.id.guess);
+        input = findViewById(R.id.input);
+        log = findViewById(R.id.log);
+
+        guess.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                doGuess();
+            }
+        });//guess setOnClick
+
+    }//onCreate
+
+    private void doGuess()
+    {
+        //取得 輸入 顯示於textView中
+        String strInput = input.getText().toString();
+        log.setText(strInput);
+
+    }//doGuess()
+
+
+}//CLASS
